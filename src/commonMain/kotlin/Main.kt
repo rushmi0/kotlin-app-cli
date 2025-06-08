cf5469d83e7aefc8e43df5df1099efc227273cfb
rushmi0 <gcalku@gmail.com>
2025-06-08 16:17:51 +0700

    update main

--- src/commonMain/kotlin/Main.kt
+++ src/commonMain/kotlin/Main.kt
@@ -1,14 +1,20 @@
 import commands.PlatformCommand
 import kotlinx.cli.*
 
-
 @OptIn(ExperimentalCli::class)
+fun run(args: Array<String>): Result<Unit> {
+    return runCatching {
+        val parser = ArgParser("kotlin-app-cli")
+        parser.subcommands(
+            PlatformCommand()
+        )
+        parser.parse(args)
+    }
+}
+
+
 fun main(args: Array<String>) {
-    val parser = ArgParser("kotlin-app-cli")
-
-    parser.subcommands(
-        PlatformCommand()
-    )
-
-    parser.parse(args)
+    run(args).onFailure { error ->
+        println("Error: ${error.message}")
+    }
 }
